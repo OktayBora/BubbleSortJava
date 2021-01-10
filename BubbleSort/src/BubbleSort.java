a581ecd23bda8d6315c961d9a14b5aae65a23e53
OktayBora <iletisim.bora@gmail.com>
2021-01-10 23:30:05 +0300

    Algoritma

---
+++ BubbleSort/src/BubbleSort.java
@@ -0,0 +1,48 @@
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		
+		int[] kapiNumarasi= {31,6,7,90,12,5,61,1,2};
+		
+		System.out.print("Normal sıralama:-\n  ");
+		
+		for (int i = 0; i < kapiNumarasi.length; i++) {
+			
+			System.out.print(kapiNumarasi[i] + ", ");
+			
+		}
+		System.out.println();
+		
+		SortKapi(kapiNumarasi);
+		
+	}
+	
+	private static void SortKapi(int[] ar) {
+		int temp;
+		
+		for (int i = ar.length-1; i>0; i--) {
+			
+			for (int j = 0; j < i; j++) {
+				if( ar[ j ]> ar[j + 1]) {
+					temp=ar[j];
+					ar[j]=ar[j+1];
+					ar[j+1]=temp;
+					
+				}
+				
+			}
+			
+		}
+		
+		
+		System.out.print("Yeni sıralama: -\n  ");
+		for (int i = 0; i < ar.length; i++) {
+			System.out.print(ar[i]+ ", ");
+			
+		}
+		System.out.println();
+		
+		
+	}
+}
